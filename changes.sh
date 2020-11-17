c324592e3e01ab830c4a27af80255e912b966568
RvSVaEk <74480072+RvSVaEk@users.noreply.github.com>
2020-11-17 15:24:49 -0800

    Update changes.sh

--- changes.sh
+++ changes.sh
@@ -19,3 +19,19 @@ sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsignedapp off
 
 # set firmware password
 sudo firmwarepasswd -setpasswd
+
+# turnoff ipv6
+sudo networksetup -setv6off "MiFi 6620L"
+
+# turn bluetooth off
+sudo defaults write /Library/Preferences/com.apple.Bluetooth ControllerPowerState -int 0
+
+# create local non-admin user
+sudo dscl . -create /Users/quota
+sudo dscl . -create /Users/quota UserShell /bin/bash
+sudo dscl . -create /Users/quota RealName "Lucius Q. User"
+sudo dscl . -create /Users/quota UniqueID "1010"
+sudo dscl . -create /Users/quota PrimaryGroupID 80
+sudo dscl . -create /Users/quota NFSHomeDirectory /Users/quota
+
+sudo dscl . -read /Users/quota GeneratedUID
